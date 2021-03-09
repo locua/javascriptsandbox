07783466f0d678de50a62398d84314d22e059c19
louis <poes997@gmail.com>
2021-03-09 13:33:54 +0000

    get json and load asynchronously

--- 2.js
+++ 2.js
@@ -1,5 +1,35 @@
 let p1 = document.createElement("P");
-p1.innerHTML=`hey this is me talking in javascript <br>
+
+p1.innerHTML=`  hey this is me talking in javascript <br>
 		big things are acoming acropper on <br>
 		the pale blue dot`
+
 document.body.append(p1)
+p1.setAttribute("id", "p1");
+
+function loadDoc() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if(this.readyState==4 && this.status == 200) {
+			document.getElementById("p1").innterHTML = this.reponseText;
+		}
+	};
+	xhttp.open("GET","notes.md");
+	xhttp.send();
+}
+//loadDoc(); // gives cross origin request error
+//const data = $.getJSON("./data.json").responseJSON;
+
+async function getData(){
+	let result;
+	try{
+	result = await $.getJSON("./data.json");
+	return result;
+	}
+	catch(error) {
+	console.log(error);
+	}
+}
+data = getData()
+data.then((value)=>{console.log(value.test)})
+
